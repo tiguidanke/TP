6211e6ace907ea30c2293b145d0e1e3bad14fe22
Deyine Jiddou <deyine.jiddou@gmail.com>
2018-12-07 20:28:40 +0000

    code error

--- cohort-model/src/main/java/sn/edacy/model/Cohort.java
+++ cohort-model/src/main/java/sn/edacy/model/Cohort.java
@@ -8,7 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.Tabl;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "cohorts")
